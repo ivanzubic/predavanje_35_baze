601180eeb9108c6ae7f16979f77ab9dbae210e05
ivanzubic <ivanzubic27@gmail.com>
2026-03-27 18:55:51 +0100

    Primary Keys

--- baza.py
+++ baza.py
@@ -8,18 +8,14 @@ c=conn.cursor()
 
 #Query database
 
-c.execute("SELECT * FROM  klijenti ")
+c.execute("SELECT rowid, * FROM  klijenti ")
 
-#print(c.fetchone()[0])
-#print(c.fetchmany(4))
-#print(c.fetchall())
 
 items=c.fetchall()
-print("Ime Prezime"+"\t"+"email")
-print("-----------"+"\t"+"-----")
+
 
 for item in items:
-    print(item[0]+" " +item [1] + "\t" +item[2])
+    print(item)
 
 
 #print("Komanda izvrsena uspjesno...")
